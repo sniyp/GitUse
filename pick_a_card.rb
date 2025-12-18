47cbe204600b9aff63489c38f37725977b71534e
sniyp <Snitcheey@yandex.ru>
2025-12-18 01:06:18 -0800

    update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,6 @@
 # тестовый комментарий с GitHub
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, черви, крести, пики
+suits = ['♦', '♠', '♥', '♣']
 
-puts "#{suits.sample}: #{values.sample}"
+puts "#{values.sample} of #{suits.sample}"
